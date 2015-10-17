eb9f0daa6be08666f9f285b89cd065ee78ca350e
Ghislain Nadeau <ghislain_nadeau@hotmail.com>
2015-10-17 18:27:47 -0400

    Modified project name... it's in fact a train manager, not the track manager

--- org.jslain.trains.track.manager.provider/src/org/jslain/trains/track/manager/provider/ITrainObservationHandlerFactory.java
+++
@@ -1,8 +0,0 @@
-package org.jslain.trains.track.manager.provider;
-
-import osgi.enroute.trains.cloud.api.Observation;
-
-public interface ITrainObservationHandlerFactory {
-
-	ITrainObservationHandler create(Observation observation);
-}
